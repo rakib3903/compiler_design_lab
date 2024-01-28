2a0f28c19b3bd1fcefe927b7ac97938f5c5b21c1
rakib3903 <rakibhossain3903@gmail.com>
2024-01-28 21:27:49 +0600

    ass_10 is done.

---
+++ ass_10.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    map<string, string>st;
+    st["S+S"]="S";
+    st["S*S"]="S";
+    // map<string, string>st = {{"S+S", "S"}, {{"S*S", "S"}}}; shortcut
+
+    freopen("file.txt", "r", stdin);
+    string s;
+    getline(cin, s);
+    
+    string parsing = "";
+
+    for(int i = 0; i < s.size(); i++){
+        if(s[i] == 'i' && s[i+1] == 'd'){
+            parsing += 'S';
+            i++;
+        }else parsing += s[i];
+    }
+
+    string p = "";
+    for(int i = 0; i < parsing.size(); i++){
+        p += parsing[i];
+        if(st[p] == "S") p = "S";
+    }
+    
+    if(p == "S")
+    cout<<"accepted"<<endl;
+    else cout<<"not accepted"<<endl;
+    return 0;
+}
